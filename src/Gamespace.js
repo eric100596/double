7f7329d338f263acd8ebe8b2b9fd2927e350acc5
Eric Baker <eric100596@aol.com>
2022-02-11 17:20:45 -0600

    added checkbox

--- src/Gamespace.js
+++ src/Gamespace.js
@@ -2,101 +2,94 @@ import React, { useState, useEffect } from 'react';
 import './Gamespace.css';
 
 
-function Gamespace(props) {
-    let deck = [
-        '0,0', '0,1', '0,2', '0,3', '0,4', '0,5', '0,6', '0,7', '1,1', '1,2', '1,3', '1,4', '1,5', '1,6', '1,7', '2,2', '2,3', '2,4', '2,5', '2,6', '2,7', '3,3' , '3,4' , '3,5', '3,6', '3,7', '4,4', '4,5', '4,6', '4,7', '5,5', '5,6', '5,7', '6,6', '6,7', '7,7'
-    ]
+// function Gamespace(props) {
+//     let deck = [
+//         '0,0', '0,1', '0,2', '0,3', '0,4', '0,5', '0,6', '0,7', '1,1', '1,2', '1,3', '1,4', '1,5', '1,6', '1,7', '2,2', '2,3', '2,4', '2,5', '2,6', '2,7', '3,3' , '3,4' , '3,5', '3,6', '3,7', '4,4', '4,5', '4,6', '4,7', '5,5', '5,6', '5,7', '6,6', '6,7', '7,7'
+//     ]
 
-    let hands;
+//     let hands;
 
-    let detNextCard = () => {
-        deck.pop();
-    }
+//     let detNextCard = () => {
+//         deck.pop();
+//     }
 
-    const [numberOfPlayers, setNumberOfPlayers] = useState(4);
+//     const [numberOfPlayers, setNumberOfPlayers] = useState(4);
 
-    useEffect(() => {
-        hands = {};
-        let numberOfCardsPerHand;
-        switch (numberOfPlayers) {
-            case 2:
-                numberOfCardsPerHand = 9;
-                break;
-            case 3:
-                numberOfCardsPerHand = 12;
-                break;
-            case 4:
-                numberOfCardsPerHand = 9;
-                break;
-        }
-    }
+//     useEffect(() => {
+//         hands = {};
+//         let numberOfCardsPerHand;
+//         switch (numberOfPlayers) {
+//             case 2:
+//                 numberOfCardsPerHand = 9;
+//                 break;
+//             case 3:
+//                 numberOfCardsPerHand = 12;
+//                 break;
+//             case 4:
+//                 numberOfCardsPerHand = 9;
+//                 break;
+//         }
+//     }
 
-    for (let i = 0; i < numberOfPlayers; i++) {
-        let hand = [];
-        for (i = 0; i < numberOfCardsPerHand; i++) {
-            hand.push(getNextCard())
-        }
+//     for (let i = 0; i < numberOfPlayers; i++) {
+//         let hand = [];
+//         for (i = 0; i < numberOfCardsPerHand; i++) {
+//             hand.push(getNextCard())
+//         }
 
-        hands[i] = hand;
-    }
+//         hands[i] = hand;
+//     }
     
-  }, [numberOfPlayers])
-
-    // function NumOfPlayers(props) {
-
-    //     const [numberOfPlayers, setNumberOfPlayers] = useState();
-    //     const handleChange = () => {
-    //         setNumberOfPlayers(0);
-    //     }
-
-    //     return(
-    //         <div>
-    //             <label>
-    //                 <input
-    //                 type="checkbox"
-    //                 numberOfPlayers={numberOfPlayers}
-    //                 onChange={handleChange}
-    //                 />
-    //                <p> How many players? "checkbox" {numberOfPlayers.toString()} </p>
-    //             </label>
-
-    //         </div>
-    //     );
-    // };
-
-    // const Checkbox = ({ type, numberOfPlayers, onChange }) => {
-    //     return(
-    //         <label>
-    //             <input type="checkbox" numberOfPlayers={numberOfPlayers} onChange={onChange} />
-    //             {type}
-    //         </label>
-    //     );
-    // };
+//   }, [numberOfPlayers])
        
+//   return (
 
-  return (
-      <>
-      <div className='playArea'>
-            <div className='logo1'>
-            <img src='Dbl-7 Logo.png'/>
-            </div>
-            
-                <div className="table">
-                    <h1>GAMESPACE</h1>
-                </div> 
-          <div className='gameControls'>
-                    <button>
-                     New Game
-                    </button>
-                    <button>
-                    Shuffle & Deal
-                    </button>
-                
-        </div>   
-    </div>
-        
-    </>
-  );
-}
+
+function Gamespace() {
+
+    const [numberOfPlayers, setNumberOfPlayers] = useState();
+
+    function handleChange(event) {
+        setNumberOfPlayers(event.target.value);
+    }
+
+    return (
+        <div> 
+            How many players?
+        <input 
+        type={Number}
+        numberOfPlayers="numberOfPlayers"
+        onChange={handleChange}
+        value={numberOfPlayers}
+        />
+
+
+<div className='playArea'>
+      <div className='logo1'>
+      <img src='Dbl-7 Logo.png'/>
+      </div>
+      
+          <div className="table">
+              <h1>GAMESPACE</h1>
+          </div> 
+    <div className='gameControls'>
+              <button>
+               New Game
+              </button>
+              <button>
+              Shuffle & Deal
+              </button>
+          
+  </div>   
+</div>
+        </div>
+
+  
+
+
+    );
+
+    }
+
 
 export default Gamespace;
